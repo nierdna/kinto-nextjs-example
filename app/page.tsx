63a69a9926c3776fb510b5b8b1f89e67ba1aef97
nierdna <cassie382.618@gmail.com>
2025-06-26 10:26:46 +0700

    Remove unused href attributes from Home component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,6 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
             onClick={kintoLogin}
@@ -70,7 +69,6 @@ export default function Home() {
             Login
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
             onClick={fetchAccountInfo}
