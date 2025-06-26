3ca556f5b8aa4a83886e83e9f89a0c9522e7605e
nierdna <cassie382.618@gmail.com>
2025-06-26 10:06:07 +0700

    Update project configuration and add account info feature
    - Updated .gitignore to include certificates.
    - Modified package.json to change the dev script and add dependencies: local-ssl-proxy and kinto-web-sdk.
    - Updated pnpm-lock.yaml to reflect new dependencies.
    - Enhanced page.module.css with styles for account information display.
    - Implemented account info fetching and login functionality in page.tsx using kinto-web-sdk.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { createKintoSDK, KintoAccountInfo } from "kinto-web-sdk";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [accountInfo, setAccountInfo] = useState<KintoAccountInfo | undefined>(
+    undefined
+  );
+  console.log("🚀 ~ Home ~ accountInfo:", accountInfo);
+
+  const kintoSDK = createKintoSDK("0x14A1EC9b43c270a61cDD89B6CbdD985935D897fE");
+
+  async function kintoLogin() {
+    try {
+      await kintoSDK.createNewWallet();
+    } catch (error) {
+      console.error("Failed to login/signup:", error);
+    }
+  }
+
+  async function fetchAccountInfo() {
+    try {
+      console.log("🚀 ~ fetchAccountInfo ~ kintoSDK:", kintoSDK);
+      setAccountInfo(await kintoSDK.connect());
+      console.log("🚀 ~ fetchAccountInfo ~ accountInfo:", accountInfo);
+    } catch (error) {
+      console.error("Failed to fetch account info:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAccountInfo();
+  });
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -26,6 +58,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={kintoLogin}
           >
             <Image
               className={styles.logo}
@@ -34,16 +67,36 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Login
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={fetchAccountInfo}
             className={styles.secondary}
           >
-            Read our docs
+            Fetch Account Info
           </a>
+        </div>
+
+        {/* Account Info Display */}
+        <div className={styles.accountInfo}>
+          {accountInfo ? (
+            <div>
+              <h2>Account Information</h2>
+              <div>
+                <p>
+                  <strong>Wallet Address:</strong> {accountInfo.walletAddress}
+                </p>
+                <p>
+                  <strong>App:</strong> {accountInfo.app?.name}
+                </p>
+              </div>
+            </div>
+          ) : (
+            <p>Please login to view account information</p>
+          )}
         </div>
       </main>
       <footer className={styles.footer}>
